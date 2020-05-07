c379bc0c79b1b8a8b70e24681f0f45bbade5fd17
Ashmi Banerjee <ashmi.banerjee@tum.de>
2020-05-08 00:25:12 +0200

    * added font awesome for linkedin * fixed Deepa name typo * changed all CET to CEST (minor fix)

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import {faTwitterSquare} from '@fortawesome/free-brands-svg-icons'
 
 export default class Person extends Component {
   render() {
@@ -18,7 +22,8 @@ export default class Person extends Component {
               </div>
               <div>{this.props.timeSlot}</div>
               <div>
-                <a href={this.props.link}>{this.props.linkedIn ? "LinkedIn" : "Instagram"}</a>
+                <a href={this.props.link}>{this.props.linkedIn ? <FontAwesomeIcon icon={faLinkedin} size={"2x"}/> : <FontAwesomeIcon icon={faInstagram} size={"2x"}/>}</a>
+                {/*<a href={this.props.link}>{this.props.linkedIn ? <FontAwesomeIcon icon={faTwitterSquare} size={"2x"}/> : <FontAwesomeIcon icon={faInstagram} size={"2x"}/>}</a>*/}
               </div>
             </div>
           </div>
