7059635ef9c308a312402aeab198522c6d063e54
eharason <harason.emily@gmail.com>
2020-05-07 21:20:51 -0400

    formatted deepa's bio

--- src/components/Jury.js
+++ src/components/Jury.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import Person from "./Person";
-import teaserImg from "../images/teaser.png";
+import ftlLogoImg from "../images/ftllogo.png";
 import deepaImg from "../images/deepa.jpg";
 
 export default class Jury extends Component {
+  deepaBio() {
+    return (
+      <div>
+        <p>
+          Deepa is heading the SAP Next-Gen Innovation Network in Munich. SAP
+          Next-Gen is a vibrant community for “innovation with purpose” related
+          to the UN Global Goals of sustainability. She is committed to driving
+          future innovation with SAP’s strategic and most innovative customers,
+          high class Universities and their Entrepreneurship Centers, as well as
+          top-notch Start Ups and successful Venture Firms.
+        </p>
+        <p>
+          With more than 19 years of experience in the software industry, Deepa
+          is also deputy chair of the Connected Mobility focus group in the
+          German Digital Economy Association (BVDW) and represents the interests
+          of SAP as a founding partner of Digital Hub Mobility in Munich.
+        </p>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div class="content">
@@ -13,18 +34,19 @@ export default class Jury extends Component {
           </div>
           <Person
             talkTitle="Deepa Gautam-Nigge"
-            bio="Deepa is heading the SAP Next-Gen Innovation Network in Munich. SAP Next-Gen is a vibrant community for “innovation with purpose” related to the UN Global Goals of sustainability. She is committed to driving future innovation with SAP’s strategic and most innovative customers, high class Universities and their Entrepreneurship Centers, as well as top-notch Start Ups and successful Venture Firms. With more than 19 years of experience in the software industry, Deepa is also deputy chair of the Connected Mobility focus group in the German Digital Economy Association (BVDW) and represents the interests of SAP as a founding partner of Digital Hub Mobility in Munich."
+            bio={this.deepaBio()}
             image={deepaImg}
             linkedIn={true}
             link={"https://www.linkedin.com/in/deepa-gautam-nigge-45235633/"}
           />
-          <Person
-            name="FTL Hackathons Team"
-            talkTitle="Judges"
-            image={teaserImg}
-            linkedIn={true}
-            link={"https://www.linkedin.com/company/female-tech-leaders/"}
-          />
+        <br/><br/>
+        <Person
+          talkTitle="FTL Hackathons Team"
+          bio={"Members from the team will also be involved in the judging!"}
+          image={ftlLogoImg}
+          linkedIn={true}
+          link={"https://www.linkedin.com/company/female-tech-leaders/"}
+        />
         </section>
       </div>
     );
