337c4ed497790c8938ad898b24c44ce60e4b2812
Emily Harason <harason.emily@gmail.com>
2020-05-18 10:16:01 -0400

    link to Johannes' linkedIn profile

--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -20,8 +20,9 @@ export default class Prizes extends Component {
     return (
       <div class="content">
         <p>
-          Artificial Intelligence will revolutionize the products and processes of the industry and also the whole society.
-          Johannes Schürmann, AI expert from INVENSITY, provides a general introduction into the topic.
+          Artificial Intelligence will revolutionize the products and processes of the industry and also the whole society.{" "}
+          <a href="https://www.linkedin.com/in/johannes-schuermann/" alt="Johannes Schürmann LinkedIn">Johannes Schürmann</a>,
+          AI expert from INVENSITY, provides a general introduction into the topic.
         </p>
         <p>
           The training Artificial Intelligence will enable the participants to have a basic understanding of what is AI,
@@ -39,7 +40,7 @@ export default class Prizes extends Component {
             <h3 className="title is-3">Prizes</h3>
           </div>
           <Person
-            name="Winning Team"
+            name={`Winning Team`}
             boldText={this.invensityPrizeTitle()}
             image={prizeAI}
             smallText={`Date: TBD ${"\u2022"} Duration: 4 hours`}
